6c3344cbecfbbc61a903189f363c64c6fdce00ec
mohmedeprahem <m.eprahem.168@gmail.com>
2023-06-08 07:04:32 +0200

    Create ui of the game

--- Program.cs
+++ Program.cs
@@ -2,16 +2,42 @@
 {
   public class Method
   {
+
+    private static void DisplalyShape(char[,] elementsGame) {
+      for(int i = 0; i < elementsGame.GetLength(0); i++) {
+        for(int j = 0; j < elementsGame.GetLength(1); j++) {
+          if (j == 2) {
+            Console.WriteLine("{0} \n----------", elementsGame[i, j]);    
+          } else {
+            Console.Write("{0} | ", elementsGame[i, j]);
+          }
+        }
+      }
+    }
     
     public static void Main()
     {
       try {
-        
-      } catch (Exception) {
+        bool isWinner = false;
+        int counter = 0;
+        char[,] elementsGame = {
+          {'1', '2', '3'},
+          {'4', '5', '6'},
+          {'7', '8', '9'}
+        };
+        // while (counter < 9) {
+        // Create ui in console
+        DisplalyShape(elementsGame);
+
+        //   // Check if there is winner
+        // }
+      } catch (IOException) {
         Console.WriteLine("Not a valid");
       }
         
     }
+
+
 
   }
 }
